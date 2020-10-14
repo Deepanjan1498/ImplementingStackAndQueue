d488bf17b62d35797e9dac420688ab8e5ac98f5f
Deepanjan1498 <deepanjansharma43@gmail.com>
2020-10-15 05:04:41 +0530

    [Deepanjan]Add . uc3 is solved and Queue is created.

---
+++ src/MyQueue.java
@@ -0,0 +1,20 @@
+
+public class MyQueue {
+		private MyLinkedList myLinkedList;
+		public MyQueue() {
+			myLinkedList = new MyLinkedList();
+		}
+
+		public INode peek() {
+			return myLinkedList.head;
+		}
+
+		public void enqueue(INode myNode) {
+			myLinkedList.append(myNode);
+		}
+
+		public void printQueue() {
+			myLinkedList.printMyNode();
+		}
+
+}
--- src/MyQueueTest.java
+++ src/MyQueueTest.java
@@ -1,4 +1,19 @@
+import static org.junit.Assert.*;
 
+import org.junit.Assert;
+import org.junit.Test;
 public class MyQueueTest {
-
+  @Test
+  public void given3NumbersWhenAddedToQueueShouldReturnQueue() {
+	  MyQueue myQueue = new MyQueue();
+	  MyNode<Integer> myFirstNode = new MyNode<>(56);
+	  MyNode<Integer> mySecondNode = new MyNode<>(30);
+	  MyNode<Integer> myThirdNode = new MyNode<>(70);
+	  myQueue.enqueue(myFirstNode);
+	  myQueue.enqueue(mySecondNode);
+	  myQueue.enqueue(myThirdNode);
+	  INode<Integer> Node = myQueue.peek();
+	  myQueue.printQueue();
+	  Assert.assertEquals(myFirstNode, Node);
+  }
 }
